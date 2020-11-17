72c09f4dbc0e2712803bae9357ec24cbe2e3b922
Hendoe <hendo1349@gmail.com>
2020-11-17 16:04:05 -0600

    seeds

---
+++ src/store.js
@@ -0,0 +1,14 @@
+const User = [
+    {
+      id: 1,
+      fullname: 'krillin',
+      username: 'best fighter number 1',
+      user_password: 'destructoD1sk',
+      email: 'turtleWarrior@roshi.com',
+      profile_photo: '/kuuuuuul',
+      about_user: 'the strongest human',
+      user_stack: 'Full Stack'
+    }
+  ];
+  
+  module.exports = bookmarks;
