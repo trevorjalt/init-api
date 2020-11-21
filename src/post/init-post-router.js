5b09ab288aa67ea9474ab67f6311598da32fa77f
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-20 19:55:02 -0600

    routes and services to get data from singular post

--- src/post/init-post-router.js
+++ src/post/init-post-router.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const InitPostService = require('./init-post-service');
 
+const { requireAuth } = require('../middleware/jwt-auth')
 const initPostRouter = express.Router();
 const jsonBodyParser = express.json();
+
+
+const serializePost = (post) => {
+
+
+    return {
+        post_id: post.id,
+        title: post.post_title,
+        live_link: post.post_live_link,
+        repository: post.post_repository,
+        tech_stack: post.tech_stack,
+        user_id: post.user_id,
+        username: post.username,
+        fullname: post.fullname
+    }
+}
 
 initPostRouter
     .route('/')
@@ -38,7 +55,7 @@ initPostRouter
                         limit: limit,
                     };
                 };
-                
+
                 //Here's the most important bit. 
                 //It slices everything and populates our results with only what we seek, i.e. the index we start at and what our limit is
                 results.results = initPosts.slice(startIndex, endIndex);
@@ -46,5 +63,30 @@ initPostRouter
                 res.json(results);
             })
     });
-    
+
+initPostRouter
+    .route('/:post_id')
+    .get(requireAuth, async (req, res, next) => {
+
+        try {
+            const post = await InitPostService.getPostById(
+                req.app.get('db'),
+                req.params.post_id
+            )
+
+            return res
+                .status(200)
+                .json(serializePost(post))
+                .end()
+
+        }
+        catch (error) {
+            next(error)
+        }
+
+
+
+    })
+
+
 module.exports = initPostRouter;
--- src/post/init-post-service.js
+++ src/post/init-post-service.js
@@ -1,7 +1,26 @@
 const InitPostService = {
-    getAllPosts(knex) {
-      return knex.select('*').from('init_posts')
-    },
-  };
-  
-  module.exports = InitPostService;
+  getAllPosts(knex) {
+    return knex.select('*').from('init_posts')
+  },
+
+  async getPostById(db, post_id) {
+    const post = await db
+      .select('*')
+      .from('init_posts')
+      .where({ id: post_id })
+
+
+    const user = await db
+      .select('username', 'fullname')
+      .from('user_information')
+      .where({ id: post.user_id })
+
+
+    return {
+      ...user,
+      ...post
+    }
+  }
+};
+
+module.exports = InitPostService;
