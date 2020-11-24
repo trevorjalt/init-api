8937c20e1ff08b2763ca9d1007bf413654f4f6a7
Ady <ady@gms-macbook-pro.lan>
2020-11-24 14:01:42 -0800

    changed avatar_id to user_id within init_follows table

--- migrations/007.do.create_init_follows.sql
+++ migrations/007.do.create_init_follows.sql
@@ -3,8 +3,8 @@ CREATE TABLE init_follows (
     date_created TIMESTAMP DEFAULT now() NOT NULL,
     username TEXT NOT NULL
         REFERENCES user_information(username) ON DELETE CASCADE,
-    avatar_id INTEGER NOT NULL
-        REFERENCES user_avatar(id) ON DELETE CASCADE,
+    user_id INTEGER NOT NULL
+        REFERENCES _information(id) ON DELETE CASCADE,
     followed_id TEXT NOT NULL ,
     activity_message VARCHAR NOT NULL,
     unread BOOLEAN DEFAULT true NOT NULL
